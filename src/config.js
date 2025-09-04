06ee1b78d3678054b4ec0a1cac9c8d6ed45dab5b
abhijeet-mhasde <168259476+abhijeet-mhasde@users.noreply.github.com>
2025-09-04 12:19:37 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
+// src/config.js
+export const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzPOwyoYoiG2xVide6Nc73jFvR52zsX_ru0x_xDsW6wLYqrTi9J9PHpXJroZgo-hcgoYg/exec"; // e.g. https://script.google.com/macros/s/…/exec
 
